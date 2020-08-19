2a69d52c639fc4d068056e410cbd056372dba0e5
rohitdhamija <rohit.dhamija@gmail.com>
2020-08-19 11:58:28 +0530

    calendar change redirect ui

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const msalConfig = {
   auth: {
     clientId: 'e5d3a39b-b8c5-4f3d-853a-1fdd345a1af8',
-    redirectUri: 'http://localhost:8081'
+    redirectUri: 'https://rohitdhamija.github.io/mscalendarview/index.html'
   },
   cache: {
     cacheLocation: "sessionStorage",
